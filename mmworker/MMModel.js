ecda8ec4053ee57ed7f06164c6dcb611543eaf2c
Craig Morris <redtree.craig@gmail.com>
2019-01-28 16:47:56 -0800

    added position arguments to model addtool command

--- mmworker/MMModel.js
+++ mmworker/MMModel.js
@@ -21,6 +21,7 @@ class MMModel extends MMTool {
 	get verbs() {
 		let verbs = super.verbs;
 		verbs['addtool'] = this.addTool;
+
 		return verbs;
 	}
 
@@ -43,17 +44,58 @@ class MMModel extends MMTool {
 
 	/** @method addTool
 	 * creates new Tool with supplied type and name
-	 *@param {MMCommand} command command.args - should be typeName and optionally name
+	 * @param {MMCommand} command command.args
+	 * args should be typeName and optionally name and x,y position
+	 * if second argument starts with a number, it is asumed to be x coordinate
 	 * @returns {MMTool}
 	 */
 	addTool(command) {
 		let parts = command.args.split(/\s/);
 		let typeName = parts[0];
-		let name = ''
-		if (parts.length > 1) {
-			name = parts[1];
+		let name;
+		let position;
+
+		/**
+		 * @function valueArg
+		 * @param {Number} i
+		 * @returns name or position or null
+		 * sees if value at parts[i] is MMPoint position value or name value
+		 * returns null if neither
+		 */
+		const valueAtArg = (i) => {
+			const re = /^[\d\.\-]+$/;
+			if (parts.length >= i+1 && re.test(parts[i]) && re.test(parts[i+1])) {
+				const x = parseFloat(parts[i]);
+				const y = parseFloat(parts[i+1]);
+				if (!isNaN(x) && !isNaN(y)) {
+					return new MMPoint(x, y); // position
+				}
+			}
+			else if (/^[\w]+$/.test(parts[i])) {
+				return parts[i];  // name
+			}
+			return null;
 		}
-		else {
+		if (parts.length > 1) {
+			const argValue = valueAtArg(1);
+			if (argValue) {
+				if (typeof argValue == 'string') {
+					name = argValue;
+					position = valueAtArg(2);
+					if (position && !(position instanceof MMPoint)) {
+						throw(this.t('mmcmd:modelInvalidPosition', {name: name, pos: position}));
+					}
+				}
+				else {	// not null or string, so must be position
+					position = argValue
+				}
+			}
+			else {
+				throw(this.t('mmcmd:modelInvalidToolName', {name: parts[1]}));
+			}
+		}
+
+		if (!name) {
 			name = `x${this.nextToolNumber++}`;
 			while ( this.childNamed(name) ) {
 				name = `x${this.nextToolNumber++}`;
@@ -63,11 +105,22 @@ class MMModel extends MMTool {
 		if(!toolType) {
 			throw(this.t('mmcmd:modelInvalidToolType', {name: name, typeName: typeName}));
 		}
-		if(toolType.factory(name, this)) {
+		let newTool = toolType.factory(name, this);
+		if (newTool) {
+			if (position) {
+				newTool.position = position;
+			}
 			command.results = true;
 			command.undo = this.getPath() + ' removechild ' + name;
 		}
 	}
+
+	/**
+	 * @method diagramInfo
+	 * @return {Object}
+	 * returns object containing the info needed for model diagram
+	 */
+	diagramInfo() {}
 
 	/**
 	 * @method forgetAllCalculations
