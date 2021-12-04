7c2d9c5d17697e3e17eeade94668816b85dda9c8
CraigGMorris <55901858+CraigGMorris@users.noreply.github.com>
2021-12-03 16:58:49 -0800

    fixed examples link in help menu

--- help/help.js
+++ help/help.js
@@ -27,7 +27,7 @@ function rtSetupHeading(title, sections) {
 	lines.push(`
 	<div class="contentlink"><a href="getstarted.html">Getting Started</a></div>
 	<div class="contentlink"><a href="tutorial.html">Tutorial</a></div>
-	<div class="contentlink"><a href="http://www.redtree.com/mmexamples">Examples</a></div>
+	<div class="contentlink"><a href="examples.html">Examples</a></div>
 	<div class="contentlink"><a href="http://www.redtree.com/contact.html">Contact Me</a></div>
 	<div class="contentlink"><a href="freeprivate.html">License</a></div>
 	<div>&nbsp;</div>
