767b39238917048150755e8fe0c1b6041ef2b62a
CraigGMorris <55901858+CraigGMorris@users.noreply.github.com>
2022-07-13 16:26:44 -0700

    Decided to remove the new code that tried to show model index tool when popping to model. Was buggy and model info views make index tools sort of obsolete

--- react/MMApp.js
+++ react/MMApp.js
@@ -565,21 +565,6 @@ export function MMApp(props) {
 						else {
 							rescale = true;
 						}
-						const indexTool = cmds && cmds[0] && cmds[0].results && cmds[0].results.indexTool;
-						if (indexTool) {
-							const path = cmds[0].results.path;
-							const newInfoState = {
-								title: indexTool,
-								path: path + '.' + indexTool,
-								modelPath: path,
-								stackIndex: infoStack.length,
-								updateCommands: '',			// commands used to update the view state
-								updateResults: [],
-								viewKey: cmds[0].results.indexToolType,
-							};
-							infoStack.push(newInfoState);
-						}
-						updateView(infoStack.length-1, rescale);
 					});
 					break;
 
