ac43fc3eca68accfe09db54cdfc377d5e793c7d1
CraigGMorris <55901858+CraigGMorris@users.noreply.github.com>
2023-12-18 20:42:48 -0800

    There was a vulnerability in the node-static package that NodeStaticServer used, so I just removed it.

--- NodeStaticServer.js
+++
@@ -1,8 +0,0 @@
-var nodeStatic = require('node-static');
-var http = require('http');
-
-var file = new(nodeStatic.Server)(__dirname);
-
-http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(8080);
