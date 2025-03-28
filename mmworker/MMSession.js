5e649023aea260424ea657f51884c9066870dcbc
CraigGMorris <55901858+CraigGMorris@users.noreply.github.com>
2025-03-28 10:38:19 -0700

    Added missing command help for session rename

--- mmworker/MMSession.js
+++ mmworker/MMSession.js
@@ -784,6 +784,7 @@ class MMSession extends MMCommandParent {
 			copy: 'mmcmd:_sessionCopy',
 			delete: 'mmcmd:_sessionDelete',
 			deleteAllSessions: 'mmcmd:_sessionDeleteAll',
+			rename: 'mmcmd:_sessionRename',
 			getjson: 'mmcmd:_sessionGetJson',
 			pushmodel: 'mmcmd:_sessionPushModel',
 			popmodel: 'mmcmd:_sessionPopModel',
